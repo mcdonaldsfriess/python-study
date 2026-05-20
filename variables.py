e5c16b76e18befa0760a326c3cc3db3f45d5a358
Soomin Lee <soominlee0306@gmail.com>
2026-05-20 11:17:20 +0900

    5/20: variables and data types

---
+++ variables.py
@@ -0,0 +1,17 @@
+# 4 basic data types in Python
+age = 20            # int
+height = 175.5      # float
+name = "Soomin"     # str
+is_student = True   # bool
+
+# Print the variables and their types
+print(age, type(age))
+print(height, type(height))
+print(name, type(name))
+print(is_student, type(is_student))
+
+# Variable reassignment and type conversion
+x = 100
+print(x, type(x))
+x = "convert to string"
+print(x, type(x))
